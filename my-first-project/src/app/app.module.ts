995704a9ce830e453983e94df20d6996fa96b190
Diversantlux <diversantlux@gmail.com>
2019-12-19 09:19:52 +0300

    Add Angular project

---
+++ my-first-project/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    ProductAlertsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ListComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-first-project/src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'iPhone ',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Samsung',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Samsunggold',
+    price: 1199,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Samsunga10',
+    price: 999,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Xaomi',
+    price: 299,
+    description: ''
+  }
+];
