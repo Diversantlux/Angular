62c3ae8ae218caa8affb000d861a5747886c4844
Diversantlux <diversantlux@gmail.com>
2020-01-14 00:21:58 +0300

    add new bottom in products.list-page

--- my-first-project/src/app/products.service.ts
+++ my-first-project/src/app/products.service.ts
@@ -9,24 +9,28 @@ export class ProductsService {
       name: 'Телек 1',
       id: '10',
       description: 'Телевизор Samsung 4k 65 display QLED ',
+      price: 200000,
       img: 'https://im0-tub-ru.yandex.net/i?id=584b635be5debfdb8f47ba6965c94b74&n=13'
     },
       {
         name: 'Телек 2',
         id: '11',
         description: 'Телевизор LG 4k 65 display OLED',
+        price: 2500000,
         img: 'https://www.electronicempire.co.uk/images/u_10180149.jpg'
       },
       {
         name: 'Телек 3',
         id: '12',
         description: 'Телевизор Samsung 4k 77 display ',
+        price: 150000,
         img: 'https://im0-tub-ru.yandex.net/i?id=8e55e9c0c8bc98619e4a90e9b4897752&n=13'
       },
       {
         name: 'Телек 4',
         id: '13',
         description: 'Телевизор LG 4k 65 display OLED 77 ',
+        price: 300000,
         img: 'https://www.technocity.ru/upload/iblock/56c/z0000127782-01.jpg'
       }],
     2: [{
--- my-first-project/src/app/products/products-list-page/products-list-page.component.ts
+++ my-first-project/src/app/products/products-list-page/products-list-page.component.ts
@@ -12,4 +12,43 @@ export class ProductsListPageComponent {
   products = this.productsService.products[this.categoryId];
   constructor(private productsService: ProductsService, private ac: ActivatedRoute) { }
 
+
+  //  const foo = function(products) {
+  //
+  //   let result = products.slice(0, products.lenght);
+  //
+  //   for (var i = 0, endI = result.length -1; i < endI; i++) {
+  //     for (var j = 0, endJ = endI - i; j < endJ; j++ ) {
+  //       if (result[j].price > result[j +1].price) {
+  //         const swap = result[j];
+  //         result[j] = result[j +1];
+  //         result[j +1] = swap;
+  //
+  //       }
+  //     }
+  //   }
+  //   return result;
+  // }
+  // foo(products);
+
+  // products = products;
+  // filter(query) {
+  //   let result = [];
+  //   if (query.length > 0) {
+  //     for (let i = 0; i < this.products.length; i++) {
+  //       const product = this.products[i];
+  //       const name = product.name;
+  //       if (name.toLowerCase().indexOf(query) > -1) {
+  //         result.push(product);
+  //       }
+  //
+  //     }
+  //   } else {
+  //     result = products;
+  //   }
+  //   this.products = result;
+  // }
+
+
+
 }
